5e53a8110e7dfed85e506814cc73c1d11ddc2cfb
Roger Zavala <rogeliozavala15@gmail.com>
2020-04-21 13:38:17 -0700

    created new routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const apiRouter = require('./server/api');
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -10,17 +11,19 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json())
 
 // Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+  })
 
 }
--- server/api.js
+++ server/api.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+apiRouter.get('/minions', (req, res, next) => {
 
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+
+});
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+
+});
+
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+
+});
 
 module.exports = apiRouter;
