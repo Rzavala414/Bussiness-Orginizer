b8d0be4fe00e2f4940f009251e680b7136084724
Roger Zavala <rogeliozavala15@gmail.com>
2020-04-22 11:41:21 -0700

    routes to ideas has been finished

--- server/ideas.js
+++ server/ideas.js
@@ -1,5 +1,5 @@
  const ideaRouter = require('express').Router();
- 
+
  const { 
     addToDatabase,
     getAllFromDatabase,
@@ -8,25 +8,44 @@
     deleteFromDatabasebyId,
   } = require('./db');
   
+  ideaRouter.param('ideaId', (req, res, next, id) => {
+        let idea = getFromDatabaseById('ideas', id);
+
+        if(!idea){
+            res.status(404).send('Idea Not Found');
+        }else{
+            req.idea = idea;
+            next();
+        }
+  });
+
  // Ideas Routes
 ideaRouter.get('/ideas', (req, res, next) => {
     res.send(getAllFromDatabase('ideas'))
 });
 
 ideaRouter.post('/ideas', (req, res, next) => {
-
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
 });
 
 ideaRouter.get('/ideas/:ideaId', (req, res, next) => {
-
+        res.send(req.idea)
 });
 
-ideaRouter.put('/ideas/ideaId', (req, res, next) => {
-
+ideaRouter.put('/ideas/:ideaId', (req, res, next) => {
+    const updateIdea = updateInstanceInDatabase('ideas', req.body)
+    res.send(updateIdea);
 });
 
 ideaRouter.delete('/ideas/:ideaId', (req, res, next) => {
+   const deleted = res.send(deleteFromDatabasebyId('ideas', req.params.id));
 
+   if(deleted){
+       res.status(204).send();
+   }else{
+       res.status(500).send();
+   }
 });
 
 module.exports = ideaRouter;
